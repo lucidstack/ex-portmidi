3c41699819edd665d5aaa247344316a42aadc0e4
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-21 19:52:06 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PortMidi.Mixfile do
 
   def project do
     [app: :portmidi,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      compilers: [:port_midi, :elixir, :app],
      build_embedded: Mix.env == :prod,
