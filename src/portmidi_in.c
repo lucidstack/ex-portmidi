3339947bfeba2db452119b830b542210480cdbad
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-26 20:40:39 +0000

    portmidi_in NIF supports multiple sources

--- src/portmidi_in.c
+++ src/portmidi_in.c
@@ -5,32 +5,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum {INPUT, OUTPUT} DeviceType;
-static PortMidiStream * stream;
+#define MAXBUFLEN 1024
 
-PmError findDevice(PmStream *stream, char *deviceName, DeviceType type);
-void debug(char*);
+typedef enum {INPUT, OUTPUT} DeviceType;
+
+PmError findDevice(PmStream **stream, char *deviceName, DeviceType type);
+const PmDeviceInfo ** listDevices(int);
+void debug(char *str);
 
 int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info) {
+  ErlNifResourceFlags flags = ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER;
+  *priv_data = enif_open_resource_type(env, NULL, "stream_resource", NULL, ERL_NIF_RT_CREATE, NULL);
+
   return 0;
 }
 
 static ERL_NIF_TERM do_open(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
   char deviceName[MAXBUFLEN];
+  ERL_NIF_TERM streamTerm;
+  PortMidiStream **streamAlloc;
   PmError result;
 
-  Pm_Initialize();
+  ErlNifResourceType* streamType = (ErlNifResourceType*)enif_priv_data(env);
+  streamAlloc = (PortMidiStream**)enif_alloc_resource(streamType, sizeof(PortMidiStream*));
 
   enif_get_string(env, argv[0], deviceName, MAXBUFLEN, ERL_NIF_LATIN1);
-  if((result = findDevice(&stream, "Launchpad Mini", INPUT)) != pmNoError) {
+  if((result = findDevice(streamAlloc, deviceName, INPUT)) != pmNoError) {
     return enif_make_badarg(env);
   }
 
-  return enif_make_atom(env, "ok");
+  streamTerm = enif_make_resource(env, streamAlloc);
+  enif_keep_resource(streamAlloc);
+
+  return enif_make_tuple2(
+    env,
+    enif_make_atom(env, "ok"),
+    streamTerm
+  );
 }
 
 static ERL_NIF_TERM do_poll(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-  if(Pm_Poll(stream)) {
+  static PortMidiStream ** stream;
+  Pm_Initialize();
+
+  ErlNifResourceType* streamType = (ErlNifResourceType*)enif_priv_data(env);
+  if(!enif_get_resource(env, argv[0], streamType, (PortMidiStream **) &stream)) {
+    return enif_make_badarg(env);
+  }
+
+  if(Pm_Poll(*stream)) {
     return enif_make_atom(env, "read");
   } else {
     return enif_make_atom(env, "retry");
@@ -40,8 +63,16 @@ static ERL_NIF_TERM do_poll(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 static ERL_NIF_TERM do_read(ErlNifEnv* env, int arc, const ERL_NIF_TERM argv[]) {
   PmEvent buffer[MAXBUFLEN];
   int status, data1, data2;
-  int numEvents = Pm_Read(stream, buffer, 1);
+  static PortMidiStream ** stream;
 
+  Pm_Initialize();
+
+  ErlNifResourceType* streamType = (ErlNifResourceType*)enif_priv_data(env);
+  if(!enif_get_resource(env, argv[0], streamType, (PortMidiStream **) &stream)) {
+    return enif_make_badarg(env);
+  }
+
+  int numEvents = Pm_Read(*stream, buffer, 1);
   status = enif_make_int(env, Pm_MessageStatus(buffer[0].message));
   data1  = enif_make_int(env, Pm_MessageData1(buffer[0].message));
   data2  = enif_make_int(env, Pm_MessageData2(buffer[0].message));
@@ -51,8 +82,8 @@ static ERL_NIF_TERM do_read(ErlNifEnv* env, int arc, const ERL_NIF_TERM argv[]) 
 
 static ErlNifFunc nif_funcs[] = {
   {"do_open", 1, do_open},
-  {"do_poll", 0, do_poll},
-  {"do_read", 0, do_read}
+  {"do_poll", 1, do_poll},
+  {"do_read", 1, do_read}
 };
 
-ERL_NIF_INIT(Elixir.PortMidi.Input,nif_funcs,load,NULL,NULL,NULL)
+ERL_NIF_INIT(Elixir.PortMidi.Input.Reader,nif_funcs,load,NULL,NULL,NULL)
