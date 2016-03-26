1bb2b999fd6ebd08d5d0d7cc449747c938231e3f
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-26 20:40:59 +0000

    add PortMidi API

--- lib/portmidi.ex
+++ lib/portmidi.ex
@@ -1,16 +1,36 @@
 defmodule PortMidi do
+  alias PortMidi.Input
+  alias PortMidi.Output
+  alias PortMidi.Listeners
+
   use Application
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(PortMidi.Listeners, []),
-      worker(PortMidi.Input,  [Application.get_env(:portmidi, :device)]),
-      worker(PortMidi.Output, [Application.get_env(:portmidi, :device)]),
+      worker(Listeners, [])
     ]
 
     opts = [strategy: :one_for_one, name: PortMidi.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def open(:input, device_name), do:
+    Input.start_link device_name
+
+  def open(:output, device_name), do:
+    Output.start_link device_name
+
+  def listen(input, pid), do:
+    Input.listen(pid, input)
+
+  def write(output, message), do:
+    Output.write(output, message)
+
+  def close(:input, input), do:
+    Input.stop(input)
+
+  def close(:output, output), do:
+    Input.stop(output)
 end
