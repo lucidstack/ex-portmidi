28d9e80697a544e9419356b885c276d0b63ba496
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-04-03 00:25:46 +0100

    bump version to 4.0.0

--- mix.exs
+++ mix.exs
@@ -1,6 +1,6 @@
 defmodule PortMidi.Mixfile do
   use Mix.Project
-  @version "3.2.0"
+  @version "4.0.0"
 
   def project do
     [app: :portmidi,
@@ -20,23 +20,11 @@ defmodule PortMidi.Mixfile do
    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {PortMidi, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:credo, "~> 0.3", only: [:dev, :test]},
      {:mock, "~> 0.1.1", only: :test},
@@ -50,8 +38,6 @@ defmodule PortMidi.Mixfile do
      licenses: ["MIT"],
      links: %{"Github" => "https://github.com/lucidstack/ex-portmidi"}]
   end
-
-
 end
 
 defmodule Mix.Tasks.Compile.PortMidi do
